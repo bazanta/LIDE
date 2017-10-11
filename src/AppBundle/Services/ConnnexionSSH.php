4bb484fa99062c0d3f7791949d4251c052049bfa
valentine <valentine.rahier@etud.univ-angers.fr>
2017-10-11 14:58:51 +0200

    mise en place mdp

--- src/AppBundle/Services/ConnnexionSSH.php
+++ src/AppBundle/Services/ConnnexionSSH.php
@@ -2,15 +2,50 @@
 
 class ConnexionSSH{
     
+    private $mdp;
+    
+    public function __construct($ssh_password){
+        
+        $this->mdp = $ssh_password;
+        
+    }
+    
 
-    public function connexionSSH(){
-       
-        $connection = ssh2_connect('localhost',8080);
-        
-      
-        
-        
-        
+    function connexionSSH(){
+
+
+	$name = 'etudiant';
+
+	$file = fopen("config.txt",'r');
+	$mdp = fgets($file);
+	fclose($file);
+
+
+	$connexion = ssh2_connect('localhost',22);
+
+	if(!$connexion){
+		echo "Connexion refusée";
+	}
+	else{
+		echo "Connexion ok";
+	}
+
+	$result = ssh2_auth_password($connexion,$name, $this->mdp);
+
+	if(!$result){
+
+		echo "Authentification refusée";
+
+	}
+	else{
+
+		echo "Authentification ok";
+
+	}
+
+	ssh2_exec($connexion,"touch ~/test");
+	ssh2_exec($connexion,"logout");
+
     }
     
     
