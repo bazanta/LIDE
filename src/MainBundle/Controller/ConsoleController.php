9930a39674bd4127dd97408ea2a4540060e3f959
valentine <valentine.rahier@etud.univ-angers.fr>
2017-10-12 18:31:27 +0200

    Mise en place de la com ssh + vue console

---
+++ src/MainBundle/Controller/ConsoleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class ConsoleController extends Controller{
+
+
+	public function indexAction(){
+
+		$content=$this->get('templating')->render('MainBundle:Console:console.html.twig',array('retourssh'=>''));
+
+		return new Response($content);
+
+
+	}
+
+	//Méthode appelée lors du clic sur le bouton Valider
+	//Fait appel à la méthode du service ConnexionSSH qui permet de traiter une commande et de récupérer la réponse à cette commande
+
+	public function traiterAction(Request $request){
+
+		$gestionssh = $this->container->get('gestionssh');
+
+		$cmd = $request->request->get('commande');
+
+
+		$retourssh = $gestionssh->traiter($cmd);
+
+		$content=$this->get('templating')->render('MainBundle:Console:console.html.twig',array('retourssh'=>$retourssh));
+
+
+
+		return new Response($content);
+
+
+
+
+	}
+
+}
+
+?>
