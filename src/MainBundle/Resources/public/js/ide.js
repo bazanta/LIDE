6ee9d1e8be6d226acd33c6e6000961707d68d8c2
Unknown <violette.paulin@gmail.com>
2017-10-23 11:22:01 +0200

    Merge branch 'feature/lang_selection_from_db' into Origine/develop

--- src/MainBundle/Resources/public/js/ide.js
+++ src/MainBundle/Resources/public/js/ide.js
@@ -1,7 +1,7 @@
 $("#console-toogle").click(function (){
-  $("#console").toggleClass("d-none");
-  $("#console").toggleClass("col-sm-4");
-  $("#console").toggleClass("col-sm-1");
+  $("#console_block").toggleClass("d-none");
+  $("#console_block").toggleClass("col-sm-4");
+  $("#console_block").toggleClass("col-sm-1");
   $("#block-editor").toggleClass("col-sm-8");
   $("#block-editor").toggleClass("col-sm-12");
   $("#console-toogle").toggleClass("console-on");
