e5727f9f7e9ac49fede2d4f5ac0e180878330017
abazante <alice.bazante@gmail.com>
2017-11-29 17:40:42 +0100

    Correction user

--- src/MainBundle/DataFixtures/ORM/userFixtures.php
+++ src/MainBundle/DataFixtures/ORM/userFixtures.php
@@ -5,31 +5,31 @@ use MainBundle\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-/**
- * Description of userFixtures
- *
- * @author etudiant
- */
-class userFixtures  extends Fixture{
-    //put your code here
-    
-     public function load(ObjectManager $manager)
-    {
-           $user = new User();
-           $user->setUsername('admin');
-	   $user->setEmail("admin@etud.univ-angers.fr");
-           $encoder = $this->container->get('security.password_encoder');
-           $password = $encoder->encodePassword($user, 'admin');
-           $user->setPassword($password);
-	   $manager->persist($user);
+class userFixtures extends Fixture
+{    
+    public function load(ObjectManager $manager)
+    {        
+        $encoder = $this->container->get('security.password_encoder');
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        //$user = new User();
+        $user = $userManager->createUser();
+        $user->setUsername('admin');
+        $user->setEmail("admin@etud.univ-angers.fr");
+        $password = $encoder->encodePassword($user, 'admin');
+        $user->setPassword($password);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_ADMIN'));
+        $manager->persist($user);
 			
-	   $user1 = new User();
-           $user1->setUsername('lide');
-	   $user1->setEmail("lide@etud.univ-angers.fr");
-           $encoder1 = $this->container->get('security.password_encoder');
-           $password1 = $encoder1->encodePassword($user1, 'lide');
-           $user1->setPassword($password1);
- 	   $manager->persist($user1);
+	    $user1 = new User();
+        $user1->setUsername('lide');
+	    $user1->setEmail("lide@etud.univ-angers.fr");
+        $password1 = $encoder->encodePassword($user1, 'lide');
+        $user1->setPassword($password1);
+        $user1->setEnabled(true);
+        $user1->setRoles(array('ROLE_USER'));
+ 	    $manager->persist($user1);
 			
 		$manager->flush();
     }
