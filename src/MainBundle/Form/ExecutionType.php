5ded8c39942e038fa1b86d720a50d9f58352b3f2
abazante <alice.bazante@gmail.com>
2018-02-03 21:18:32 +0100

    Suite amélioration fixtures

--- src/MainBundle/Form/ExecutionType.php
+++ src/MainBundle/Form/ExecutionType.php
@@ -7,63 +7,63 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
+use  MainBundle\Entity\Execution;
+
 
 class ExecutionType extends AbstractType
 {
 
-  public function buildForm(FormBuilderInterface $builder, array $options)
-   {
-       $builder
-           ->add('inputMode', ChoiceType::class, array(
-             'label' => "Gestion des entrées",
-             'choices' => array(
-               'none' => 'Aucune',
-               'it' => 'Interractive',
-               'text' => 'Texte'
-              ),
-              'expanded' => true,
-              'multiple' => false,
-            )
-           )
-           ->add('inputs', TextareaType::class)
-           ->add('compileOnly', CheckboxType::class, array(
-            'label' => 'Compilation uniquement',
-           ))
-           ->add('launchParameters', TextType::class, array(
-             'label' => 'Paramètres de lancement'
-           ))
-           ->add('compilationOptions', TextType::class, array(
-             'label' => 'Option de compilation',
-             'data' => '-Wall'
-           ))
-           ->add('files', HiddenType::class)
-           ->add('language', HiddenType::class)
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('inputMode', ChoiceType::class, array(
+                'label' => "Gestion des entrées",
+                'choices' => array(
+                    'none' => 'Aucune',
+                    'it' => 'Interractive',
+                    'text' => 'Texte'
+                ),
+                'expanded' => true,
+                'multiple' => false,
+            ))
+            ->add('inputs', TextareaType::class)
+            ->add('compileOnly', CheckboxType::class, array(
+                'label' => 'Compilation uniquement',
+            ))
+            ->add('launchParameters', TextType::class, array(
+                'label' => 'Paramètres de lancement'
+            ))
+            ->add('compilationOptions', TextType::class, array(
+                'label' => 'Options de compilation',
+                'data' => '-Wall'
+            ))
+            ->add('files', HiddenType::class)
+            ->add('language', HiddenType::class)
             ->add('additionalFiles', FileType::class, array(
                 'label' => 'Fichiers additionnels',
                 'multiple' => true
             ));
-   }
+    }
 
-   public function configureOptions(OptionsResolver $resolver)
-   {
-       $resolver->setDefaults(array(
-           'data_class' => 'MainBundle\Entity\Execution'
-       ));
-   }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Execution::class,
+        ));
+    }
 
-   /**
-    * {@inheritdoc}
-    */
-   public function getBlockPrefix()
-   {
-       return 'mainbundle_execution';
-   }
+    /**
+        * {@inheritdoc}
+        */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_execution';
+    }
 };
 ?>
---
+++ src/MainBundle/Resources/script/exec_c.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+function testExtension {
+	case $1 in
+    "c" | "cpp" | "cxx" | "hpp" | "C" | "H" | "hh" | "o")
+                echo 1
+                ;;
+    *)
+      echo 0
+    ;;
+	esac
+}
+
+fichierInput=""
+
+compilationUniquement=false
+
+arguments=""
+
+wgetAdr=""
+
+echo -e "\e[33m$(date) Starting ...\e[0m";
+
+while getopts "o:f:i:nca:w:" option
+do
+	case $option in
+		o)
+			options=${OPTARG}
+			;;
+		f)
+			fichiers=${OPTARG}
+			;;
+    i)
+      fichierInput=${OPTARG}
+    	;;
+		n)
+			touch input
+			fichierInput="input"
+		;;
+		a)
+			arguments=${OPTARG}
+			;;
+    c)
+      compilationUniquement=true
+      ;;
+		w)
+			wgetAdr=${OPTARG}
+	esac
+done
+
+listeFichiers=$(echo $fichiers | tr " " "\n")
+
+for fichier in $listeFichiers
+do
+	wget $wgetAdr$fichier 2>/dev/null
+done
+
+listeFichiersFinale=""
+
+for fichier in $listeFichiers
+do
+	#on récupère l'extension du fichier pour savoir s'il faut l'ajouter aux fichiers à compiler ou non
+	extension=$(echo $fichier | rev | cut -d "." -f1 | rev)
+	retourTestExtension=$(testExtension $extension)
+
+	if [ $retourTestExtension -eq 1 ]
+	then
+		listeFichiersFinale="$listeFichiersFinale $fichier"
+	fi
+done
+
+echo -e "\e[1;33m$(date)\$ gcc $options $listeFichiersFinale \e[0m\n Press Enter to start"
+gcc $options $listeFichiersFinale
+resCompil="$?"
+read x
+if [ "$resCompil" != "0" ]
+then
+    exit $resCompil
+fi
+
+
+executable=$(ls -t | head -1)
+
+
+if [ $compilationUniquement = false ]
+then
+	if [ -x $executable ]
+	then
+	  case $fichierInput in
+	  	"")
+			echo -e "\e[33m$(date)\$ ./$executable $arguments \e[0m"
+	  		./$executable $arguments
+	  		res=$?
+	  		;;
+	  	*)
+					echo -e "\e[33m$(date)\$ ./$executable $arguments \e[0m"
+	  		./$executable $arguments < $fichierInput
+	  		res=$?
+	  		;;
+	  esac
+	fi
+	if [ "$res" = "0" ]
+	then
+      echo -e "\e[1;32m$(date)\$ Process finished with exit code $res\e[0m"
+    else
+      echo -e "\e[1;31m$(date)\$ Process finished with exit code $res\e[0m"
+    fi
+  if [ -f input ]
+  then
+  	rm input
+  fi
+
+fi
